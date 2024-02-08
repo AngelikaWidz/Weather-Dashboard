e178f120e1caff6589e4d03162295b6c2a95290a
AngelikaWidz <angelikawidz@yahoo.com>
2024-02-08 14:52:14 +0000

    Added variables

---
+++ assets/index.js
@@ -0,0 +1,6 @@
+const apiKey = 'KEY';
+const searchForm = document.getElementById('search-form');
+const cityInput = document.getElementById('search-input');
+const currentWeatherDiv = document.getElementById('currentWeather');
+const forecastDiv = document.getElementById('forecast');
+const historyList = document.getElementById('historyList');
