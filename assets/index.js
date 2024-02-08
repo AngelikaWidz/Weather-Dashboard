e6d52526bb14d6bd7851a3c17dfb74eff7f140d7
AngelikaWidz <angelikawidz@yahoo.com>
2024-02-08 15:05:21 +0000

    started event lister

--- assets/index.js
+++ assets/index.js
@@ -4,3 +4,16 @@ const cityInput = document.getElementById('search-input');
 const currentWeatherDiv = document.getElementById('currentWeather');
 const forecastDiv = document.getElementById('forecast');
 const historyList = document.getElementById('historyList');
+
+// add event listner for submit event
+searchForm.addEventListener('submit', function (event) {
+    // handle submission
+    event.preventDefault();
+    const city = cityInput.value;
+
+    // Call API to get weather data
+    getWeatherData(city);
+
+    // Clear input field
+    cityInput.value = '';
+  });
